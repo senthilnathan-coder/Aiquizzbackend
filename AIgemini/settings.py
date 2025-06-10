275899e13ebb351c126eaa2a878ad15f6ca959ad
ArunkumarT03 <arunkumart0310@gmail.com>
2025-06-10 15:02:10 +0530

    10-06-25

--- AIgemini/settings.py
+++ AIgemini/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://6842b5d00c029a9a1950199b--digiaiquestt.netlify.app"
+    "https://6842b5d00c029a9a1950199b--digiaiquestt.netlify.app",
 ]
 
 
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',  # Uncomment this line
+    'django.middleware.csrf.CsrfViewMiddleware',  
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
