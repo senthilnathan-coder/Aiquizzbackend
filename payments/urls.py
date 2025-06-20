e1ff370bc93d44b17c10a5b36def07acf2d03103
ArunkumarT03 <arunkumart0310@gmail.com>
2025-06-20 16:43:55 +0530

    20-06-25

--- payments/urls.py
+++ payments/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from payments.views import *
 
 urlpatterns = [
-    path('plans/create/', CreateSubscriptionPlanView.as_view()),
-    path('plans/', ListSubscriptionPlansView.as_view()),
-    path('payment/create/', CreateSubscriptionOrderView.as_view()),
-    path('payment/verify/', VerifySubscriptionPaymentView.as_view()),
+    path('plans/create/', CreateSubscriptionPlanView.as_view(),name='plans_create'),
+    path('plans/', ListSubscriptionPlansView.as_view(),name='plans'),
+    path('payment/create/', CreateSubscriptionOrderView.as_view(),name='payment_create'),
+    path('payment/verify/', VerifySubscriptionPaymentView.as_view(),name='payment_verify'),
 ]
