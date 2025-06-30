0674c8a150fb0a765e73f920e7aca1a24e3d9cf7
ArunkumarT03 <arunkumart0310@gmail.com>
2025-06-30 13:28:39 +0530

    30-06-25

--- AIgemini/urls.py
+++ AIgemini/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('app2/',include('app2.urls')),
     path('app3/',include('app3.urls')),
     path('payments/',include('payments.urls')),
-    path('',include('flashcard.urls'))
+    path('api',include('flashcard.urls'))
 ]
